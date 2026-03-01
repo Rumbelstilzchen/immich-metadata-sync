33bd8d76c48e4661005091e30d66da9fad4df7f8
germanlion67 <143081644+germanlion67@users.noreply.github.com>
2026-03-01 21:33:44 +0100

    Update immich-backup.sh

--- script/backup/immich-backup.sh
+++ script/backup/immich-backup.sh
@@ -312,7 +312,6 @@ backup_library() {
     log "   🔄 Synchronisiere Dateien (kann einige Zeit dauern)..."
     
     if ! rsync -ah --info=progress2 --delete --checksum \
-        #--exclude=".*" \
         --exclude="Thumbs.db" \
         --exclude=".DS_Store" \
         "$IMMICH_LIBRARY_DIR/" \
